9e5a7ac7f3de0018ae33ac0cff58679335b092a1
Eldon de Aquino Costa <eldon.costa@compasso.com.br>
2022-01-19 23:36:47 -0300

    prisma workshop

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,57 @@ const app = express();
 app.use(express.json());
 
 app.get("/users", async (req, res) => {
-  // const result = TODO
-  // res.json(result)
+  const result = await prisma.user.findMany();
+  res.json(result)
+
 });
 
 app.post(`/signup`, async (req, res) => {
   const { name, email } = req.body;
 
-  // const result = TODO
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email
+    }
+  })
 
-  // res.json(result)
+  res.json(result)
 });
 
 app.post(`/post`, async (req, res) => {
   const { title, content, authorEmail } = req.body;
 
-  // const result = TODO
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: {
+        connect: {
+          email: authorEmail
+        }
+      }
+    }
+  })
 
-  // res.json(result)
+  res.json(result)
 });
 
 app.put("/post/:id/views", async (req, res) => {
   const { id } = req.params;
 
-  // const result = TODO
+  const result = await prisma.post.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      viewCount: {
+        increment: 1
+      }
+    }
+  })
 
-  // res.json(result)
+  res.json(result)
 });
 
 app.put("/publish/:id", async (req, res) => {
